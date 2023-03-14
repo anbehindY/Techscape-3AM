bec03edb8b94520864520f5e88e1fb79c58f4fe2
anbehindY <yanminthwin@gmail.com>
2023-03-15 01:50:38 +0630

    Fix linter errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('.menuButton');
 const header = document.querySelector('header');
 
-menu.addEventListener('click', ()=> {
-    menu.classList.toggle('active');
-    header.classList.toggle('active');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  header.classList.toggle('active');
 });
